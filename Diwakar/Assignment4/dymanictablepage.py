647d96a2f59737c948f66d1f9b620c98d1e18b57
rajesh <rksingh29@gmail.com>
2022-08-21 11:00:14 +0530

    Delete Diwakar directory

--- Diwakar/Assignment4/dymanictablepage.py
+++
@@ -1,9 +0,0 @@
-class DymanictablePage:
-
-    def __init__(self, driver):
-        self.driver = driver
-        self.table_xpath = "//table[@class='dataTable']"
-
-    @property
-    def table(self):
-        return self.driver.find_element_by_xpath(self.table_xpath)
